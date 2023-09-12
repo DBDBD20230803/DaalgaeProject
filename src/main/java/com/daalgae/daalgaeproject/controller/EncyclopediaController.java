0fb7827455f11c0f3c39da77a5c00c2667a2bb57
ksostuft <ksostuft@gmail.com>
2023-09-12 15:59:46 +0900

    테스트

---
+++ src/main/java/com/daalgae/daalgaeproject/controller/EncyclopediaController.java
@@ -0,0 +1,33 @@
+package com.daalgae.daalgaeproject.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class EncyclopediaController {
+    @GetMapping("/encycleMain")
+    public String pediaMain() {
+        return "/daaalgeEncyclopedia/encyclopediaMain";
+    }
+
+    @GetMapping("knowBeforeAdopt")
+    public String KnowBeforeAdopt() {
+        return "/daaalgeEncyclopedia/knowBeforeAdopt";
+    }
+
+    @GetMapping("knowAfterAdopt")
+    public String KnowAfterAdopt() {
+        return "/daaalgeEncyclopedia/knowAfterAdopt";
+    }
+
+    @GetMapping("knowEmergency")
+    public String KnowEmergency() {
+        return "/daaalgeEncyclopedia/knowEmergency";
+    }
+
+    @GetMapping("petiket")
+    public String Petiket() {
+        return "/daaalgeEncyclopedia/petiket";
+    }
+}
---
+++ src/main/java/com/daalgae/daalgaeproject/controller/TourController.java
@@ -0,0 +1,13 @@
+package com.daalgae.daalgaeproject.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class TourController {
+    @GetMapping("/tour")
+    public String TourMain() {
+        return "/tour/tour";
+    }
+}
