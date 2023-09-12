e46151fdf7fc233edc3e4890a25e43519f71cadd
ksostuft <ksostuft@gmail.com>
2023-09-12 19:41:44 +0900

    문제를 해결하기 위해 직접 merge를 하기로 했습니다.

--- src/main/java/com/daalgae/daalgaeproject/config/SecurityConfig.java
+++ src/main/java/com/daalgae/daalgaeproject/config/SecurityConfig.java
@@ -3,10 +3,10 @@ package com.daalgae.daalgaeproject.config;
 import com.daalgae.daalgaeproject.login.service.AuthenticationService;
 import com.daalgae.daalgaeproject.login.service.LoginService;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @Configuration
-@EnableWebSecurity
+//@EnableWebSecurity
 public class SecurityConfig {
 
     private LoginService loginService;
--- src/main/java/com/daalgae/daalgaeproject/login/controller/LoginController.java
+++ src/main/java/com/daalgae/daalgaeproject/login/controller/LoginController.java
@@ -4,23 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-/*
-@Controller
-@RequestMapping("/login/*")
-public class LoginController {
 
+@Controller
+@RequestMapping("/login")
+public class LoginController {
     @GetMapping("/login")
-    public String loginForm(){
-        return "login/login";
-    }
-
-}
-*/
-
-@Controller
-@RequestMapping("/")
-public class LoginController {
-    @GetMapping("/")
     public String loginForm(){
         return "login/login";
     }
