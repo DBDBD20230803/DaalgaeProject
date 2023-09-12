e2b14485b9cbcff79f11dcb5d2ca6bd718a60eee
ksostuft <ksostuft@gmail.com>
2023-09-12 18:23:32 +0900

    merge 테스트

--- src/main/java/com/daalgae/daalgaeproject/board/controller/EncyclopediaController.java
+++ src/main/java/com/daalgae/daalgaeproject/board/controller/EncyclopediaController.java
@@ -1,4 +1,4 @@
-package com.daalgae.daalgaeproject.controller;
+package com.daalgae.daalgaeproject.board.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
