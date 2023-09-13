3d0ab016f965b3656ad0acc4b521da481ffc244b
peaknicc <ybkz17@naver.com>
2023-09-13 18:43:55 +0900

    긴급통합

---
+++ src/main/java/com/daalgae/daalgaeproject/board/controller/BoardAnnoController.java
@@ -0,0 +1,20 @@
+package com.daalgae.daalgaeproject.board.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/board/*")
+public class BoardAnnoController {
+
+    @GetMapping("/announcementBoardSelect")
+    public String selectAnno() { return "/board/announcementBoardSelect"; }
+
+    @GetMapping("/announcementBoardWrite")
+    public String writeAnno() { return "/board/announcementBoardWrite"; }
+
+//    @GetMapping("/announcementBoardModify")
+//    public String modifyAnno() { return "/board/announcementBoardModify"; }
+
+}
--- src/main/java/com/daalgae/daalgaeproject/board/controller/BoardController.java
+++ src/main/java/com/daalgae/daalgaeproject/board/controller/BoardController.java
@@ -2,20 +2,25 @@ package com.daalgae.daalgaeproject.board.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/board/*")
 public class BoardController {
 
+    @GetMapping("/mainBoard")
+    public String boardMain() { return "/board/mainBoard"; }
 
-    @GetMapping("/Board/freeBoard")
+    @GetMapping("/freeBoard")
     public String freeBoardMain() { return "/board/freeBoard"; }
 
-    @GetMapping("/Board/boastBoard")
+    @GetMapping("/boastBoard")
     public String boastBoardMain() { return "/board/boastBoard"; }
 
-    @GetMapping("/Board/abandonedBoard")
+    @GetMapping("/abandonedBoard")
     public String abandonedBoardMain() { return "/board/abandonedBoard"; }
 
-    @GetMapping("/Board/announcementBoard")
+    @GetMapping("/announcementBoard")
     public String announcementBoardMain() { return "/board/announcementBoard"; }
+
 }
