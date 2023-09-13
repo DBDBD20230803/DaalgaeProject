f9827a3be9109c5a13a3654fb5311d953cdd1425
lsuns <j76067469@gmail.com>
2023-09-13 19:18:24 +0900

    20230913-sh-005

---
+++ src/main/java/com/daalgae/daalgaeproject/webtoon/controller/WebtoonController.java
@@ -0,0 +1,24 @@
+package com.daalgae.daalgaeproject.webtoon.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("webtoon")
+public class WebtoonController {
+
+
+    @GetMapping("dengInfo")
+    public String goWebtoonPage() {
+        System.out.println("aaa");
+        return "webtoon/dengInfo";
+    }
+
+    @GetMapping("webtoonDetail")
+    public String goWebtoonDetailPage(){
+        return "webtoon/webtoonDetail";
+    }
+
+
+}
