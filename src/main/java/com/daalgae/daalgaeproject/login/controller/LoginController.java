1c73193006d890867d1429ce820e331ac24717e9
somang1206 <dlthakd1206@naver.com>
2023-09-14 17:34:05 +0900

    패키지 경로 정리

--- src/main/java/com/daalgae/daalgaeproject/login/controller/LoginController.java
+++ src/main/java/com/daalgae/daalgaeproject/login/controller/LoginController.java
@@ -2,15 +2,23 @@ package com.daalgae.daalgaeproject.login.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
 @Controller
 @RequestMapping("/login")
 public class LoginController {
+
+
     @GetMapping("/login")
     public String loginForm(){
         return "login/login";
     }
 
+    @GetMapping("/loginFindId")
+    public String loginFindIdForm(){ return "/login/loginFindId"; }
+
+    @GetMapping("/loginFindPwd")
+    public String loginFindFPwdForm(){ return "/login/lgoinFindPwd"; }
 }
--- src/main/java/com/daalgae/daalgaeproject/login/model/service/LoginServiceImpl.java
+++ src/main/java/com/daalgae/daalgaeproject/login/model/service/LoginServiceImpl.java
@@ -15,6 +15,7 @@ import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 import java.util.List;*/
 
+
 /*
 @Service
 public class LoginServiceImpl implements LoginService{
@@ -53,4 +54,5 @@ public class LoginServiceImpl implements LoginService{
         return user;
     }
 }
+
 */
