659d2837fe0c037b31259a2158fcb567dab7149c
peaknicc <ybkz17@naver.com>
2023-09-15 17:18:33 +0900

    자유게시판 목록 조회 기능 구현 테스트 성공

--- src/main/java/com/daalgae/daalgaeproject/board/controller/BoardController.java
+++ src/main/java/com/daalgae/daalgaeproject/board/controller/BoardController.java
@@ -1,13 +1,29 @@
 package com.daalgae.daalgaeproject.board.controller;
 
+import com.daalgae.daalgaeproject.board.dto.BoardDTO;
+import com.daalgae.daalgaeproject.board.service.BoardServiceImpl;
+import com.daalgae.daalgaeproject.common.paging.Pagenation;
+import com.daalgae.daalgaeproject.common.paging.SelectCriteria;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/board/*")
 public class BoardController {
+
+    private final BoardServiceImpl boardServiceImpl;
+
+    public BoardController(BoardServiceImpl boardServiceImpl) {
+        this.boardServiceImpl = boardServiceImpl;
+    }
 
     @GetMapping("/")
     public ModelAndView testBoard(ModelAndView mv) {
@@ -16,14 +32,41 @@ public class BoardController {
     }
 
     @GetMapping("/freeBoard")
-    public ModelAndView freeBoardMain(ModelAndView mv) {
+    public ModelAndView freeBoardMain(HttpServletRequest request
+                                    , @RequestParam(required = false) String searchCondition
+                                    , @RequestParam(required = false) String searchValue
+                                    , @RequestParam(value="currentPage", defaultValue = "1") int pageNo
+                                        , ModelAndView mv) {
+
+        Map<String, String> searchMap = new HashMap<>();
+        searchMap.put("searchCondition", searchCondition);
+        searchMap.put("searchValue", searchValue);
+
+        int totalCount = boardServiceImpl.selectTotalCount(searchMap);
+
+        int limit = 10;
+
+        int buttonAmount = 5;
+
+        SelectCriteria selectCriteria = null;
+
+        if(searchCondition != null && !"".equals(searchCondition)) {
+            selectCriteria = Pagenation.getSelectCriteria(pageNo, totalCount, limit, buttonAmount, searchCondition, searchValue);
+        } else {
+            selectCriteria = Pagenation.getSelectCriteria(pageNo, totalCount, limit, buttonAmount);
+        }
+
+        List<BoardDTO> boardList = boardServiceImpl.selectBoardList(selectCriteria);
+
+        mv.addObject("boardList", boardList);
+        mv.addObject("selectCriteria", selectCriteria);
+
         mv.setViewName("board/freeBoard");
         return mv;
     }
 
     @GetMapping("/boastBoard")
     public ModelAndView boastBoardMain(ModelAndView mv) {
-        System.out.println("무슨일이야!");
         mv.setViewName("board/boastBoard");
         return mv;
     }
@@ -36,7 +79,6 @@ public class BoardController {
 
     @GetMapping("/announcementBoard")
     public ModelAndView announcementBoardMain(ModelAndView mv) {
-        System.out.println("공지사항 페이지로 이동~");
         mv.setViewName("board/announcementBoard");
         return mv;
     }
