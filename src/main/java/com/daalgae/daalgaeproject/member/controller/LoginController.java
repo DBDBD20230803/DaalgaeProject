ed5e4843cb89fdf2e1b22fa4bc00167f04608b62
somang1206 <dlthakd1206@naver.com>
2023-09-15 14:33:25 +0900

    20230914-lsm-06 로그인 기능 구현 진행

--- src/main/java/com/daalgae/daalgaeproject/member/controller/LoginController.java
+++ src/main/java/com/daalgae/daalgaeproject/member/controller/LoginController.java
@@ -1,8 +1,7 @@
-package com.daalgae.daalgaeproject.login.controller;
+package com.daalgae.daalgaeproject.member.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
