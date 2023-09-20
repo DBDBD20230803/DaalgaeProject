d82a8fae09658c3b4a0b6cbe1f86095d58136931
somang1206 <dlthakd1206@naver.com>
2023-09-20 20:39:58 +0900

    20230920-lsm-03 깃허브 버그사항 수정

---
+++ src/main/java/com/daalgae/daalgaeproject/exception/member/EmailAuthException.java
@@ -0,0 +1,7 @@
+package com.daalgae.daalgaeproject.exception.member;
+
+public class EmailAuthException extends Exception{
+    public EmailAuthException(){}
+
+    public EmailAuthException(String msg){ super(msg); }
+}
