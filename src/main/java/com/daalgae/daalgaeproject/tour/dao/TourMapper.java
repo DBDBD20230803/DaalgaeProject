451b3f6aa8e666402aa29351d61bdc80a99842eb
ksostuft <ksostuft@gmail.com>
2023-09-21 11:25:05 +0900

    20230920-ksh-01 Tour 기능 만드는 중

--- src/main/java/com/daalgae/daalgaeproject/tour/dao/TourMapper.java
+++ src/main/java/com/daalgae/daalgaeproject/tour/dao/TourMapper.java
@@ -1,7 +1,11 @@
 package com.daalgae.daalgaeproject.tour.dao;
 
+import com.daalgae.daalgaeproject.tour.dto.TourDTO;
 import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
 
 @Mapper
 public interface TourMapper {
+    List<TourDTO> findTourList(String tourList);
 }
