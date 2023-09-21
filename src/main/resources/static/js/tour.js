146966ee0d05cb041fac1c12eb969f815dcfa0d2
ksostuft <ksostuft@gmail.com>
2023-09-21 19:44:04 +0900

    20230921-khs-05 이제 메뉴에 들어갈 때 분야에 맞는 마커가 뜹니다.

--- src/main/resources/static/js/tour.js
+++ src/main/resources/static/js/tour.js
@@ -75,7 +75,6 @@ $(document).ready( function() {
             $('.mapSearch').css("left", "-370px");
         }
     });
-
     // 마커 관련
     $(function(){
         w = window.innerWidth - 100;
@@ -90,37 +89,40 @@ $(document).ready( function() {
         if(userX && userY) {
             options = {
                 center: new kakao.maps.LatLng(userX, userY),
-                level: 5
+                level: 10
             };
         } else {
             options = {
-                center: new kakao.maps.LatLng(34.54513393, 126.8998852),
-                level: 5
+                center: new kakao.maps.LatLng(37.5652352, 126.992384),
+                level: 10
             };
         }
 
         let map = new kakao.maps.Map(container, options);
         map.relayout();
-
         $.ajax({
             type:"get",
-            url:"tour/getTourKakaoMap",
+            url:"/tour/getTourKakaoMap",
             dataType:"json",
+            async: false,
             success: function(data){
-                let positions = new Array(206);
+                localStorage.setItem("isData", data);
+                let tourLocInfo = new Array(206);
                 console.log(data);
-                for(let i=0; i<206; i++) {
-                    positions[i] = {
+                for(let i= 0; i<data.length; i++) {
+                    tourLocInfo[i] = {
                         title : data[i].tourTitle,
-                        latlng: new kakao.maps.LatLng(data[i].mapy, data[i].mapx)
+                        latlng: new kakao.maps.LatLng(data[i].mapy, data[i].mapx),
+                        category: data[i].tourCategory
                     }
                 }
+                console.log(tourLocInfo);
 
                 // 마커 이미지의 이미지 주소입니다
                 let imageSrc = "https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png";
 
-                for (let i = 0; i < positions.length; i ++) {
-
+                for (let i = 0; i < tourLocInfo.length; i ++) {
+                    console.log(tourLocInfo[i]);
                     // 마커 이미지의 이미지 크기 입니다
                     let imageSize = new kakao.maps.Size(24, 35);
 
@@ -128,17 +130,69 @@ $(document).ready( function() {
                     let markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize);
 
                     // 마커를 생성합니다
-                    let marker = new kakao.maps.Marker({
-                        map: map, // 마커를 표시할 지도
-                        position: positions[i].latlng, // 마커를 표시할 위치
-                        title : positions[i].title, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
-                        image : markerImage // 마커 이미지
-                    });
+                    if(selectNum == 1) {
+                        if(tourLocInfo[i].category.includes("관광")) {
+                            let marker = new kakao.maps.Marker({
+                                map: map, // 마커를 표시할 지도
+                                position: tourLocInfo[i].latlng, // 마커를 표시할 위치
+                                title : tourLocInfo[i].title, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
+                                image : markerImage // 마커 이미지
+                            });
+                        }
+                    } else if(selectNum == 2) {
+                        if(tourLocInfo[i].category.includes("숙박")) {
+                            let marker = new kakao.maps.Marker({
+                                map: map, // 마커를 표시할 지도
+                                position: tourLocInfo[i].latlng, // 마커를 표시할 위치
+                                title : tourLocInfo[i].title, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
+                                image : markerImage // 마커 이미지
+                            });
+                        }
+                    } else if(selectNum == 3) {
+                        if(tourLocInfo[i].category.includes("식음료")) {
+                            let marker = new kakao.maps.Marker({
+                                map: map, // 마커를 표시할 지도
+                                position: tourLocInfo[i].latlng, // 마커를 표시할 위치
+                                title : tourLocInfo[i].title, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
+                                image : markerImage // 마커 이미지
+                            });
+                        }
+                    } else if(selectNum == 4) {
+                        if(tourLocInfo[i].category.includes("체험")) {
+                            let marker = new kakao.maps.Marker({
+                                map: map, // 마커를 표시할 지도
+                                position: tourLocInfo[i].latlng, // 마커를 표시할 위치
+                                title : tourLocInfo[i].title, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
+                                image : markerImage // 마커 이미지
+                            });
+                        }
+                    } else if(selectNum == 5) {
+                        if(tourLocInfo[i].category.includes("동물병원")) {
+                            let marker = new kakao.maps.Marker({
+                                map: map, // 마커를 표시할 지도
+                                position: tourLocInfo[i].latlng, // 마커를 표시할 위치
+                                title : tourLocInfo[i].title, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
+                                image : markerImage // 마커 이미지
+                            });
+                        }
+                    } else {
+                        let marker = new kakao.maps.Marker({
+                            map: map, // 마커를 표시할 지도
+                            position: tourLocInfo[i].latlng, // 마커를 표시할 위치
+                            title : tourLocInfo[i].title, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
+                            image : markerImage // 마커 이미지
+                        });
+                    }
+
+                    // 마커 스타일 달기
+
                 }
             },
             error:function(){
                 console.log("통신에러3");
             }
         });
+
+
     });
 });
