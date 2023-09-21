dc6d6f95b1df274c3cec51749d83df5ffdbea1d1
ksostuft <ksostuft@gmail.com>
2023-09-21 16:36:14 +0900

    20230921-khs-03 지도에 마커 다는 중입니다.

--- src/main/resources/static/js/tour.js
+++ src/main/resources/static/js/tour.js
@@ -1,9 +1,12 @@
 $(document).ready( function() {
-    $('#headers').css("position", "fixed");
-    $('#headers').css("width", "100%");
-    $('#headers').css("z-index", "5");
-    $('#menu').css("margin-top", "-16px");
+    $(window).on('resize', function() {
+        w = window.innerWidth - 100;
+        h = window.innerHeight - 100;
+        $('#map').css('width',w);
+        $('#map').css('height',h);
+    });
 
+    // 체크 관련, 검색 관련
     let selectNum = $('input[name=selectNum]').val();
     if(selectNum == 0) {
         $('#tourCheck input').prop('checked', 'true');
@@ -71,5 +74,88 @@ $(document).ready( function() {
         } else {
             $('.mapSearch').css("left", "-370px");
         }
-    })
+    });
+
+    // 마커 관련
+    $(function(){
+        w = window.innerWidth - 100;
+        h = window.innerHeight - 100;
+        $('#map').css('width',w);
+        $('#map').css('height',h);
+
+        let container = document.getElementById('map');
+        let options;
+        let userX = Number(localStorage.getItem("userX"));
+        let userY = Number(localStorage.getItem("userY"));
+        if(userX && userY) {
+            options = {
+                center: new kakao.maps.LatLng(userX, userY),
+                level: 5
+            };
+        } else {
+            options = {
+                center: new kakao.maps.LatLng(34.54513393, 126.8998852),
+                level: 5
+            };
+        }
+
+        let map = new kakao.maps.Map(container, options);
+        map.relayout();
+
+        let a;
+        $.ajax({
+            type:"get",
+            url:"tour/getTourKakaoMap",
+            dataType:"json",
+            success: function(data){
+                console.log(data);
+                var positions;
+                for(let datum of data) {
+                    let positions = Array(206);
+                    positions = [
+                        {
+                            title: datum.tourTitle,
+                            latlng: new kakao.maps.LatLng(33.450705, 126.570677)
+                        },
+                        {
+                            title: '생태연못',
+                            latlng: new kakao.maps.LatLng(33.450936, 126.569477)
+                        },
+                        {
+                            title: '텃밭',
+                            latlng: new kakao.maps.LatLng(33.450879, 126.569940)
+                        },
+                        {
+                            title: '근린공원',
+                            latlng: new kakao.maps.LatLng(33.451393, 126.570738)
+                        }
+                    ];
+                }
+
+
+                // 마커 이미지의 이미지 주소입니다
+                var imageSrc = "https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png";
+
+                for (var i = 0; i < positions.length; i ++) {
+
+                    // 마커 이미지의 이미지 크기 입니다
+                    var imageSize = new kakao.maps.Size(24, 35);
+
+                    // 마커 이미지를 생성합니다
+                    var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize);
+
+                    // 마커를 생성합니다
+                    var marker = new kakao.maps.Marker({
+                        map: map, // 마커를 표시할 지도
+                        position: positions[i].latlng, // 마커를 표시할 위치
+                        title : positions[i].title, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
+                        image : markerImage // 마커 이미지
+                    });
+                }
+            },
+            error:function(){
+                console.log("통신에러3");
+            }
+        });
+    });
 });
