79908a90b2c18d42c1f40344ba79943ab1a29297
ksostuft <ksostuft@gmail.com>
2023-09-21 18:07:55 +0900

    20230921-khs-04 마커 지도에 달았습니다.

--- src/main/resources/static/js/tour.js
+++ src/main/resources/static/js/tour.js
@@ -102,50 +102,33 @@ $(document).ready( function() {
         let map = new kakao.maps.Map(container, options);
         map.relayout();
 
-        let a;
         $.ajax({
             type:"get",
             url:"tour/getTourKakaoMap",
             dataType:"json",
             success: function(data){
+                let positions = new Array(206);
                 console.log(data);
-                var positions;
-                for(let datum of data) {
-                    let positions = Array(206);
-                    positions = [
-                        {
-                            title: datum.tourTitle,
-                            latlng: new kakao.maps.LatLng(33.450705, 126.570677)
-                        },
-                        {
-                            title: '생태연못',
-                            latlng: new kakao.maps.LatLng(33.450936, 126.569477)
-                        },
-                        {
-                            title: '텃밭',
-                            latlng: new kakao.maps.LatLng(33.450879, 126.569940)
-                        },
-                        {
-                            title: '근린공원',
-                            latlng: new kakao.maps.LatLng(33.451393, 126.570738)
-                        }
-                    ];
+                for(let i=0; i<206; i++) {
+                    positions[i] = {
+                        title : data[i].tourTitle,
+                        latlng: new kakao.maps.LatLng(data[i].mapy, data[i].mapx)
+                    }
                 }
 
-
                 // 마커 이미지의 이미지 주소입니다
-                var imageSrc = "https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png";
+                let imageSrc = "https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png";
 
-                for (var i = 0; i < positions.length; i ++) {
+                for (let i = 0; i < positions.length; i ++) {
 
                     // 마커 이미지의 이미지 크기 입니다
-                    var imageSize = new kakao.maps.Size(24, 35);
+                    let imageSize = new kakao.maps.Size(24, 35);
 
                     // 마커 이미지를 생성합니다
-                    var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize);
+                    let markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize);
 
                     // 마커를 생성합니다
-                    var marker = new kakao.maps.Marker({
+                    let marker = new kakao.maps.Marker({
                         map: map, // 마커를 표시할 지도
                         position: positions[i].latlng, // 마커를 표시할 위치
                         title : positions[i].title, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
