64c679492269fd86201cc52062f9a19913e07a9c
ksostuft <ksostuft@gmail.com>
2023-09-23 13:03:53 +0900

    20230923-khs-02 header는 일단 롤백했습니다. 나중에 merge한 다음에 분리를 할 지 말 지 결정하겠습니다.

--- src/main/resources/static/js/header.js
+++ src/main/resources/static/js/header.js
@@ -4,9 +4,4 @@ $(document).ready( function() {
     $('#headers').css("z-index", "5");
     $('#menu').css("margin-top", "-16px");
     $('.font12 button').css("font-size", '12px');
-    $("#headerSearchBox").on("keyup",function(key){
-        if(key.keyCode==13) {
-            alert($("#headerSearchBox").val());
-        }
-    });
 });
