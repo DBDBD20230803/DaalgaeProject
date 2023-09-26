478e660384ab127f0c431a5517411ef550294250
ksostuft <ksostuft@gmail.com>
2023-09-26 16:46:14 +0900

    20230926-khs-08 공지사항 데이터 불러오는 거 가능합니다.

--- src/main/java/com/daalgae/daalgaeproject/mainConnect/controller/mainConnectController.java
+++ src/main/java/com/daalgae/daalgaeproject/mainConnect/controller/mainConnectController.java
@@ -1,23 +1,21 @@
 package com.daalgae.daalgaeproject.mainConnect.controller;
 
 import com.daalgae.daalgaeproject.mainConnect.dto.MainNoticeDTO;
-import com.daalgae.daalgaeproject.mainConnect.service.mainConnectService;
+import com.daalgae.daalgaeproject.mainConnect.service.MainConnectService;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
 @Controller
-@RequestMapping("tour")
+@RequestMapping(value = {"main", "/"})
 public class mainConnectController {
-    private final mainConnectService mainConnectService;
+    private final MainConnectService mainConnectService;
 
-    public mainConnectController(mainConnectService mainConnectService) {
+    public mainConnectController(MainConnectService mainConnectService) {
         this.mainConnectService = mainConnectService;
     }
 
--- src/main/java/com/daalgae/daalgaeproject/mainConnect/service/MainConnectService.java
+++ src/main/java/com/daalgae/daalgaeproject/mainConnect/service/MainConnectService.java
@@ -1,12 +1,12 @@
 package com.daalgae.daalgaeproject.mainConnect.service;
 
 import com.daalgae.daalgaeproject.mainConnect.dto.MainNoticeDTO;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
-public interface mainConnectService {
-
+public interface MainConnectService {
     List<MainNoticeDTO> findNotice();
 }
